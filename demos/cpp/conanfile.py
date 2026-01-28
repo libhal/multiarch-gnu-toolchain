160fe201aabe8bb85f2c6ce020e4f3a562f1f6b4
Khalil Estell <kammce0@gmail.com>
2026-01-28 05:35:32 -0800

    Upgrade demos to latest in llvm

--- demos/cpp/conanfile.py
+++ demos/cpp/conanfile.py
@@ -1,16 +1,28 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain, CMakeDeps
 
 
-class Demo(ConanFile):
+class BasicCppConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv"
+    generators = "VirtualBuildEnv"
 
     def requirements(self):
         pass
 
+    def build_requirements(self):
+        self.tool_requires("ninja/1.13.2")
+        self.tool_requires("cmake/[>=3.28.0 <5.0.0]")
+
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generator = "Ninja"
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
